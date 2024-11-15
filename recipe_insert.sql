d857a01548b8e7b626fe7cf282e7ee08e98ef46e
hyerin lee <helena1005@naver.com>
2024-11-15 17:59:53 +0900

    lhr : 시퀀스 추가

--- recipe_insert.sql
+++ recipe_insert.sql
@@ -1,4 +1,12 @@
 insert into recipecategory values(category_seq.nextval, '한식', '탕/찌개');
 insert into recipecategory values(category_seq.nextval, '한식', '밥');
 insert into recipecategory values(category_seq.nextval, '한식', '반찬');
-insert into recipecategory values(category_seq.nextval, '한식', '김치');
+insert into recipecategory values(category_seq.nextval, '일식', '밥');
+insert into recipecategory values(category_seq.nextval, '일식', '면');
+insert into recipecategory values(category_seq.nextval, '일식', '간식');
+insert into recipecategory values(category_seq.nextval, '중식', '면');
+insert into recipecategory values(category_seq.nextval, '중식', '밥');
+insert into recipecategory values(category_seq.nextval, '중식', '요리');
+insert into recipecategory values(category_seq.nextval, '양식', '면');
+insert into recipecategory values(category_seq.nextval, '양식', '밥');
+insert into recipecategory values(category_seq.nextval, '양식', '요리');
