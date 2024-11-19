846d2698f5c73928bd8e70ce0214f22940a57842
sia cheon <jysa0512@naver.com>
2024-11-19 10:01:41 +0900

    csa : MemberDTO

---
+++ src/main/java/com/mbc/cook/repository/member/MemberRepository2.java
@@ -0,0 +1,18 @@
+package com.mbc.cook.repository.member;
+
+import com.mbc.cook.entity.member.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository2 extends JpaRepository<MemberEntity,String> {
+    @Transactional
+    @Query(value = "select id from usermember where id = :id", nativeQuery = true)
+    String idcheck(@Param("id") String id);
+}
---
+++ src/main/java/com/mbc/cook/service/member/MemberInterface2.java
@@ -0,0 +1,14 @@
+package com.mbc.cook.service.member;
+
+public interface MemberInterface2 {
+    public String getId();
+    public String getPw();
+    public String getName();
+    public String getJumin1();
+    public String getJumin2();
+    public String getAddr();
+    public String getStreetaddr();
+    public String getDetailaddr();
+    public String getTel();
+    public String getEmail();
+}
