623531937379ff287f3fa35e0ba96570eee8e174
sia cheon <jysa0512@naver.com>
2024-11-19 11:43:39 +0900

    csa : 회원가입 repository

--- src/main/java/com/mbc/cook/repository/member/MemberRepository2.java
+++ src/main/java/com/mbc/cook/repository/member/MemberRepository2.java
@@ -2,13 +2,10 @@ package com.mbc.cook.repository.member;
 
 import com.mbc.cook.entity.member.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
 public interface MemberRepository2 extends JpaRepository<MemberEntity,String> {
