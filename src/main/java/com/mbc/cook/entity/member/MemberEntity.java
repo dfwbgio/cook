cc2dd739c229f30cfdae905255e129bd249b4aad
sia cheon <jysa0512@naver.com>
2024-11-19 11:42:56 +0900

    csa : MemberEntity

--- src/main/java/com/mbc/cook/entity/member/MemberEntity.java
+++ src/main/java/com/mbc/cook/entity/member/MemberEntity.java
@@ -24,7 +24,10 @@ public class MemberEntity {
     private String name;
 
     @Column
-    private String jumin;
+    private String jumin1;
+
+    @Column
+    private String jumin2;
 
     @Column
     private String addr;
@@ -36,22 +39,34 @@ public class MemberEntity {
     private String detailaddr;
 
     @Column
-    private String tel;
+    private String tel1;
 
     @Column
-    private String email;
+    private String tel2;
+
+    @Column
+    private String tel3;
+
+    @Column
+    private String email_id;
+    @Column
+    private String email_domain;
 
     @Builder
-    public MemberEntity(String id, String pw, String name, String jumin, String addr, String streetaddr, String detailaddr, String tel, String email) {
+    public MemberEntity(String id, String pw, String name, String jumin1, String jumin2, String addr, String streetaddr, String detailaddr, String tel1,String tel2,String tel3, String email_id, String email_domain) {
         this.id = id;
         this.pw = pw;
         this.name = name;
-        this.jumin = jumin;
+        this.jumin1 = jumin1;
+        this.jumin2 = jumin2;
         this.addr = addr;
         this.streetaddr = streetaddr;
         this.detailaddr = detailaddr;
-        this.tel = tel;
-        this.email = email;
+        this.tel1 = tel1;
+        this.tel2 = tel2;
+        this.tel3 = tel3;
+        this.email_id = email_id;
+        this.email_domain = email_domain;
     }
 
 }
