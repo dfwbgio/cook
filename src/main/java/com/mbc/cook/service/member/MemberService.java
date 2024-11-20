de6d2c73cd7e69322146283673f380bd9a51e812
phj5196 <hjun8342@naver.com>
2024-11-20 16:39:34 +0900

    phj : 로그인 Service

--- src/main/java/com/mbc/cook/service/member/MemberService.java
+++ src/main/java/com/mbc/cook/service/member/MemberService.java
@@ -3,16 +3,17 @@ package com.mbc.cook.service.member;
 import com.mbc.cook.dto.member.MemberDTO;
 import com.mbc.cook.entity.member.MemberEntity;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface MemberService {
     List<MemberEntity> getid(String name, String tel, String email);
 
-    List<MemberEntity> getpw(String id, String name, String tel, String email);
+    int getpw(String id, String name, String tel, String email);
 
     void pwupdate(String id, String pw);
 
-    List<MemberEntity> UserControl();
+    Page<MemberEntity> UserControl(int page);
+
 }
--- src/main/java/com/mbc/cook/service/member/MemberServiceImp.java
+++ src/main/java/com/mbc/cook/service/member/MemberServiceImp.java
@@ -1,19 +1,22 @@
 package com.mbc.cook.service.member;
 
-import com.mbc.cook.dto.member.MemberDTO;
 import com.mbc.cook.entity.member.MemberEntity;
 import com.mbc.cook.repository.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
 public class MemberServiceImp implements MemberService{
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public List<MemberEntity> getid(String name, String tel, String email) {
@@ -21,18 +24,19 @@ public class MemberServiceImp implements MemberService{
     }
 
     @Override
-    public List<MemberEntity> getpw(String id, String name, String tel, String email) {
-        return memberRepository.findPW(id,name,tel,email);
+    public int getpw(String id, String name, String tel, String email) {
+        return memberRepository.findPW(id, name, tel, email);  // findPW 메서드 호출
     }
 
     @Override
     public void pwupdate(String id, String pw) {
-        memberRepository.pwupdate(id,pw);
+        String encryptedPW = bCryptPasswordEncoder.encode(pw);
+        memberRepository.pwupdate(id,encryptedPW);
     }
 
     @Override
-    public List<MemberEntity> UserControl() {
-        return memberRepository.findAll();
+    public Page<MemberEntity> UserControl(int page) {
+        return memberRepository.findAll(PageRequest.of(page,10, Sort.by(Sort.Direction.DESC,"id")));
     }
 
 
